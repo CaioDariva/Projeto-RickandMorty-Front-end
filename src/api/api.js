72d9816f7a32230a48b9b991f77fb311f0920dcd
Caio <cgmalavazidariva@gmail.com>
2021-09-06 23:05:53 -0300

    arrumando as rotas no front

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 export const Api = {
 	baseUrl: "https://backend-projeto-rickandmorty.herokuapp.com/personagens",
 
-	createUrl: () => Api.baseUrl + "/",
+	createUrl: () => Api.baseUrl + "/create",
 
-	readAllUrl: () => Api.baseUrl + "/",
-	readSingleUrl: (id) => Api.baseUrl + "/" + id,
+	readAllUrl: () => Api.baseUrl + "/read-all",
+	readSingleUrl: (id) => Api.baseUrl + "/read-by-id" + id,
 
-	updateUrl: (id) => Api.baseUrl + "/" + id,
+	updateUrl: (id) => Api.baseUrl + "/update" + id,
 
-	deleteUrl: (id) => Api.baseUrl + "/" + id,
+	deleteUrl: (id) => Api.baseUrl + "/delete" + id,
 	deleteAllUrl: () => Api.baseUrl + "/",
 
 	// Create
